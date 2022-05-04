75371213b5df22bba8536797f036de62a12530da
Rogerio Lopes <crogeriolopes@gmail.com>
2022-05-04 18:05:16 -0300

    alt. init script

--- deploy_niture.sh
+++ deploy_niture.sh
@@ -5,6 +5,12 @@ sudo yum install httpd -y
 sudo apachectl start
 sudo systemctl enable httpd
 
+#sudo firewall-cmd --zone=public --add-service=http
+#sudo firewall-cmd --permanent --zone=public --add-service=http
+
+sudo systemctl stop firewalld
+sudo systemctl desable firewalld
+
 cd /var/www/html/
 
 sudo wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/u8j40_AS-7pRypC5boQT24w5QFPDTy-0j27BWBOfmsxbERTiuDtJQBIqfcsOH81F/n/idqfa2z2mift/b/bootcamp-oci/o/oci-f-handson-modulo-compute-website-files.zip
@@ -14,7 +20,3 @@ sudo unzip oci-f-handson-modulo-compute-website-files.zip
 sudo chown -R apache:apache /var/www/html
 
 sudo rm -rf oci-f-handson-modulo-compute-website-files.zip
-
-#-- Error reported running soon after 'systemctl start httdp'
-sudo firewall-cmd --zone=public --add-service=http
-sudo firewall-cmd --permanent --zone=public --add-service=http
