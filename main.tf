7868966408314091c911aa6f5210708cf2411791
Rogerio Lopes <crogeriolopes@gmail.com>
2022-05-05 13:10:40 -0300

    LB-9 corr

--- main.tf
+++ main.tf
@@ -211,7 +211,7 @@ resource "oci_load_balancer_load_balancer" "load_balancer" {
     #Required
     compartment_id = var.compartment_ocid
     display_name = "lb-wbsrv-ha"
-    shape = "10 Mbps"    # = "10Mbps-Micro"
+    shape = "10"    # = "10Mbps-Micro"
     subnet_ids = [oci_core_subnet.tcb_subnet.id]
 
     #Optional
