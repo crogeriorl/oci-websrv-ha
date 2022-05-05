0e1e43737f45abc50ddbd72f49ece7dbc52c3f8d
Rogerio Lopes <crogeriolopes@gmail.com>
2022-05-05 12:57:19 -0300

    LB.5 corr.

--- main.tf
+++ main.tf
@@ -211,7 +211,7 @@ resource "oci_load_balancer_load_balancer" "load_balancer" {
     #Required
     compartment_id = var.compartment_ocid
     display_name = "lb-wbsrv-ha"
-    shape = "10Mbps-Micro"    # = "Flexible"
+    shape = "Flexible"    # = "10Mbps-Micro"
     subnet_ids = [oci_core_subnet.tcb_subnet.id]
 
     #Optional
@@ -224,11 +224,11 @@ resource "oci_load_balancer_load_balancer" "load_balancer" {
         #Optional
         # id = var.load_balancer_reserved_ips_id
     # }
-    # shape_details {
-        #Required
-        # maximum_bandwidth_in_mbps = "10"
-        # minimum_bandwidth_in_mbps = "10"
-    # }
+    shape_details {
+      #Required
+      maximum_bandwidth_in_mbps = "5"
+      minimum_bandwidth_in_mbps = "5"
+    }
 }
 
 
