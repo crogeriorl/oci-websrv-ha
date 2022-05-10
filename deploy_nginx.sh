6b7ee9021d8c03447bfe4a86e50a592a8ce3b867
Rogerio Lopes <crogeriolopes@gmail.com>
2022-05-10 10:11:36 -0300

    nginx demo

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+
 sudo yum install nginx -y
 
-#sudo systemctl start nginx
-#sudo systemctl enable nginx
 
-#$ The commands below don't worked on cloud-init script for OCI Oracle-Linux-7.9-2020.10.26-0
+# The commands below don't worked on cloud-init script for OCI Oracle-Linux-7.9-2020.10.26-0
+
 #sudo firewall-cmd --zone=public --add-service=http
 #sudo firewall-cmd --permanent --zone=public --add-service=http
 
@@ -13,6 +13,25 @@ sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 
 
-# Inclui host e IP na pagina index.html
+# Install Nginx demo files on base directories
 
-sudo sed -i '24i '"<h3><br><FONT COLOR="#0000FF">Load Balancer + H.A. / $(hostname -f) - IP: $(hostname -i)</FONT><br></h3>"'' /var/www/html/index.nginx-debian.html
+cd /etc/nginx/conf.d
+sudo rm -rf hello.conf
+sudo wget https://raw.githubusercontent.com/crogeriorl/NGINX-Demos/master/nginx-hello/hello.conf
+
+cd /usr/share/nginx/html/
+sudo mv index.html index.html.orig
+sudo rm -rf index.html
+sudo wget https://raw.githubusercontent.com/crogeriorl/NGINX-Demos/master/nginx-hello/index.html
+
+
+# Insert some text, host and IP within index.html
+
+sudo sed -i '88i '"<h1><br><FONT COLOR="#0000FF"><center>## OCI - Always Free: 2 VMs w/ Load Balancer + H.A.</center></FONT><br></h3>"'' /usr/share/nginx/html/index.html
+sudo sed -i '88i '"<h3><br><FONT COLOR="#0000FF"><center>$(hostname -f) - IP: $(hostname -i)</center></FONT><br></h3>"'' /usr/share/nginx/html/index.html
+
+
+# Initialize NGINX service
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
