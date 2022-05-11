3e39e3728ba3f6230f60e2b1d7fbce6e55928e90
Rogerio Lopes <crogeriolopes@gmail.com>
2022-05-11 09:59:55 -0300

    id update

--- autoscaling.tf
+++ autoscaling.tf
@@ -7,7 +7,7 @@ resource "oci_autoscaling_auto_scaling_configuration" "auto_scaling_configuratio
     }
     compartment_id = var.compartment_ocid
     policies {
-        display_name = "autoscaling-CPUusage-policy"
+        display_name = "autoscaling-threshold-policy"
         is_enabled   = "true"
  
         capacity {
